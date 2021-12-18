abdb008ba0941f267c899a6ea22f5e1d3dc8d2b4
Diego <diegorf55@gmail.com>
2021-12-18 09:35:48 -0300

    inserindo cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app =  express();
 const filmes = require('./src/data/filmes.json')
+const cors = require('cors');
 
 
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get('/filmes', (req, res) => {
     return res.json(filmes);
