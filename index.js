03fb06c3a4682f342cbf99c896a9bccdbb3c0dd3
Diego <diegorf55@gmail.com>
2021-12-18 09:38:55 -0300

    configurando cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app =  express();
 const filmes = require('./src/data/filmes.json')
+
+//permite acesso, configurando as urls que são permitidas para acessar a aplicação
 const cors = require('cors');
 
 
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors({origin: 'https://api-filmes-nodejs.herokuapp'}))
 
 app.get('/filmes', (req, res) => {
     return res.json(filmes);
