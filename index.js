cbe15dd566c9cead572edf28916753a9bba80261
Diego <diegorf55@gmail.com>
2021-12-18 09:29:47 -0300

    aterado o start para npm

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app =  express();
 const filmes = require('./src/data/filmes.json')
-const cors = require('cors');
+
 
 const port = process.env.PORT || 3000;
 
-
-app.use(cors());
-
 app.get('/', (req, res) => {
    
-    res.send("Olá Diego")
+    res.send("Olá Diego");
 });
 
 app.listen(port, () => {
